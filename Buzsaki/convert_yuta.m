adddc680c2679e0fb3f33f88006efc0453da8864
bendichter <ben.dichter@gmail.com>
2018-04-02 15:13:59 -0700

    conversion progress

--- Buzsaki/convert_yuta.m
+++ Buzsaki/convert_yuta.m
@@ -2,9 +2,11 @@ addpath(genpath(pwd));
 
 registry=generateCore('schema/core/nwb.namespace.yaml');
 
-%%
+%% file
 
 fpath = '/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMouse41-150903';
+
+
 [fpath_base, fname, ext] = fileparts(fpath);
 session_description = 'mouse in open exploration and theta maze';
 identifier = fname;
@@ -36,7 +38,7 @@ ss = types.SpatialSeries('source',{'position sensor0'},...
     'data',aa(:,[1,2]),'timestamps',(1:length(aa))*fs,...
     'starting_time',0);
 
-file.acquisition.sensor_0 = ss;
+file.acquisition.position_sensor_0 = ss;
 
 
 ss = types.SpatialSeries('source',{'position sensor1'},...
@@ -44,13 +46,16 @@ ss = types.SpatialSeries('source',{'position sensor1'},...
     'data',aa(:,[3,4]),'timestamps',(1:length(aa))*fs,...
     'starting_time',0);
 
-file.acquisition.sensor_1 = ss;
+file.acquisition.position_sensor_1 = ss;
 
 %% load LFP
 lfp_filepath = fullfile(fpath, [fname, '.lfp']);
 lfp_file = fopen(lfp_filepath, 'r');
 lfp_data = fread(lfp_file,'int16=>int16'); % takes 45 seconds 
 lfp_data = reshape(lfp_data,[],sessionInfo.nChannels);
+
+%%
+lfp_data = lfp_data(1:100,:);
 lfp_tt = (1:length(lfp_data)) * samplingRate;
 
 %% construct LFP
@@ -84,6 +89,68 @@ for i=1:length(channel_groups)
     file.acquisition.(['shank' num2str(i) ' lfp']) = es;
 end
 
+%% special elctrodes
+
+special_electrode_names = {'ch_wait', 'ch_arm', 'ch_solL', 'ch_solR',...
+    'ch_dig1', 'ch_dig2', 'ch_entL', 'ch_entR', 'ch_SsolL', 'ch_SsolR'};
+special_electrode_numbers = [79,78,76,77,65,68,72,71,73,70];
+
+for i=1:length(special_electrode_names)
+    name = special_electrode_names{i};
+    channel = special_electrode_numbers(i);
+    ts = types.TimeSeries(...
+        'source',{'a hypothetical source'},...
+        'timestamps', lfp_tt',...
+        'data',lfp_data(:,channel+1));
+    
+    file.acquisition.(name) = ts;
+
+end
+
+%% Spike times
+
+
+
+%% Cell Types
+
+load(fullfile(fpath_base,'DG_all_6__UnitFeatureSummary_add.mat'))
+this_file = all(UnitFeatureCell.fname == fname, 2);
+
+celltype_keys = UnitFeatureCell.fineCellType(this_file);
+region_keys = UnitFeatureCell.region(this_file);
+unit_id = UnitFeatureCell.unitID(this_file);
+
+
+% taken from ReadMe
+celltype_dict = containers.Map([0:6,8:10],...
+    {'unknown',...
+    'granule cells (DG) or pyramidal cells (CA3)  (need to use region info. see below.)',...
+    'mossy cell',...
+    'narrow waveform cell',...
+    'optogenetically tagged SST cell',...
+    'wide waveform cell (narrower, exclude opto tagged SST cell)',...
+    'wide waveform cell (wider)',...
+    'positive waveform unit (non-bursty)',...
+    'positive waveform unit (bursty)',...
+    'positive negative waveform unit'});
+
+region_dict = containers.Map(3:4, {'CA3','DG'});
+
+
+celltype_names = {};
+for i=1:length(celltype_keys)
+    if celltype_keys(i) == 1
+        if region_keys(i) == 3
+            celltype_names{i} = 'pyramidal cell';
+        elseif region_keys(i) == 4
+            celltype_names{i} = 'granule cell';
+        end
+    else
+        celltype_names{i} = celltype_dict(celltype_keys(i));
+    end
+end
+
+[u_cats, ~, indices] = unique(celltype_names);
 
 %%
 
